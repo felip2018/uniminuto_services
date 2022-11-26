2fc1cc7655ca32cb09a74fbaa05e295fdbfe54ff
felip2018 <felipegarxon@hotmail.com>
2022-11-25 23:22:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,81 @@
 'use strict';
+
 module.exports.handler = (event, context, callback) => {
-    let requestBody = JSON.parse(event.body);
-    console.log("Request Body:: ", requestBody);
+    console.log("EVENTO > ", event);
     
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
-        body: JSON.stringify(requestBody)
+    let response;
+    
+    if (event.path === "/uniminuto_services/get-pokemons") {
+        
+        let requestBody = {
+            message: "Welcome to get pokemons",
+            result: getPokemonsList()
+        }
+        
+        response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify(requestBody)
+        }
+        
+        callback(null, response);
     }
     
-    callback(null, response);
+    if (event.path === "/uniminuto_services/login") {
+        
+        console.log('Launch login');
+        
+        response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify(event.body)
+        }
+        
+        callback(null, response);
+    }
+    
+    
+}
+
+const getPokemonsList = () => {
+    return {
+        list: [
+            {
+                "name": "bulbasaur",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/1/"
+            },
+            {
+                "name": "ivysaur",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/2/"
+            },
+            {
+                "name": "venusaur",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/3/"
+            },
+            {
+                "name": "charmander",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/4/"
+            },
+            {
+                "name": "charmeleon",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/5/"
+            },
+            {
+                "name": "charizard",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/6/"
+            },
+            {
+                "name": "squirtle",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/7/"
+            },
+            {
+                "name": "wartortle",
+                "url": "https://pokeapi.co/api/v2/pokemon-species/8/"
+            }
+        ]
+    }
 }
